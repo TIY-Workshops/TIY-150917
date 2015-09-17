6a1d891649b5c81108600cefee7f62161fa9a81f
DarynHolmes <DarynHolmes@gmail.com>
2015-09-17 19:41:55 +0100

    Basic logic in place

--- game.rb
+++ game.rb
@@ -1,1 +1,42 @@
+
+# MUST HAVE
+
+# Iteration 1
+# Welcoming the user - done!
+# get the users name
+# get the users choice
+# generate the computers (bots) choice - hard coded for now
+# Evaluate result to establish who won
+
+# Iteration 2
+# Random computer choice
+
+# NICE TO HAVE
+
+# Validate the users input (was it a valid choice?)
+# Explain the rules
+# Repeat the game
+
+
 puts "welcome to Rock Paper Scissors!!!"
+
+print "Please enter your name:"
+name = gets.chomp
+
+print "Please make your choice r,p,s:"
+players_choice = gets.chomp
+
+computer_choice = "r"
+
+if ("s" == players_choice)
+	puts "Computer wins!"
+elsif ("p" == players_choice)
+	puts "Player wins!!!!!! woot!!!!"
+else
+	puts "Draw!!!!!"
+end
+
+
+
+
+
